1fe3a0d50f1d796d38f0e21978eb24cabe760000
CullyLine <Linebergercully@gmail.com>
2024-05-21 03:46:02 -0400

    hi

--- Main.ts
+++ Main.ts
@@ -2,8 +2,8 @@ import { app, BrowserWindow } from 'electron'
 
 const createWindow = () => {
     const win : BrowserWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
+      width: 1280,
+      height: 720,
     })
   
     win.loadFile('index.html')
